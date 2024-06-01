c8e035e206c9615976f9f4c90f7d4f1a8a95798d
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-01 17:25:00 +0900

    groupRoutes.js meetingRoutes.js 파일 주석 변경

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -5,10 +5,10 @@ const authenticateJWT = require('../middleware/authenticateJWT');
 
 const router = express.Router();
 
-// GET : http://localhost:3000/api/groups : 팀원 정보 조회
+// GET : http://localhost:3000/api/groups : 인증된 사용자가 속한 그룹과 그룹 멤버 정보를 조회
 router.get('/', authenticateJWT, getGroup);
 
-// GET : http://localhost:3000/api/groups/shedule : 팀원들의 시간표 정보 조회
+// GET : http://localhost:3000/api/groups/shedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
 router.get('/schedule', authenticateJWT, getSchedule);
 
 module.exports = router;
