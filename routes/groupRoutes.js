70076eaa6453db314518b29b8dfdc686da37d9c2
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-01 17:10:13 +0900

    groupRoutes.js파일 주석 추가

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -5,7 +5,10 @@ const authenticateJWT = require('../middleware/authenticateJWT');
 
 const router = express.Router();
 
+// GET : http://localhost:3000/api/groups : 팀원 정보 조회
 router.get('/', authenticateJWT, getGroup);
+
+// GET : http://localhost:3000/api/groups/shedule : 팀원들의 시간표 정보 조회
 router.get('/schedule', authenticateJWT, getSchedule);
 
 module.exports = router;
