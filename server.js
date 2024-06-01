e5c1d8521e9d883b5294d439c2c5770d3196a1b1
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-01 16:44:29 +0900

    server.js파일 수정

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // 서버 초기화 및 설정 파일 
 const express = require('express');
 const bodyParser = require('body-parser');
-const authRoutes = require('./routes/authRoutes');
+const authRoutes = require('./routes/authRoutes.js');
 const groupRoutes = require('./routes/groupRoutes');
 const meetingRoutes = require('./routes/meetingRoutes');
 const dotenv = require('dotenv');
@@ -20,5 +20,19 @@ app.use('/api/meetings', meetingRoutes);
 const PORT = process.env.PORT; // .env 파일에서 불러오기 
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`서버가 포트${PORT}에서 진행중`);
 });
+
+
+
+app.get('/', (req, res) => {
+  res.send('여기는 로그인 페이지입니다');
+}) 
+
+app.get('/main', (req, res) => {
+  res.send('여기는 메인 페이지입니다');
+}) 
+
+app.get('/main/group', (req, res) => {
+  res.send('여기는 그룹 페이지입니다');
+}) 
