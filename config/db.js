46ae2df357db384be773f2ec12b32310b5cd9499
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-01 15:58:05 +0900

    db.js 파일 추가

---
+++ config/db.js
@@ -0,0 +1,19 @@
+// 데이터베이스 연결 설정 
+const mysql = require('mysql2');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,    // .env파일로부터 가져오기
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log('Connected to the MySQL server.');
+});
+
+module.exports = connection;
