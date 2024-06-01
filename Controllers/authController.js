00cef9551a02c6fd52ab9a9613193e9b69fba2df
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-01 16:05:11 +0900

    Controllers 폴더에 파일들 추가

---
+++ Controllers/authController.js
@@ -0,0 +1,20 @@
+const User = require('../models/User');
+const { generateToken } = require('../utils/jwt');
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+  User.findByEmail(email, (err, user) => {
+    if (err || !user) {
+      return res.status(401).send('해당 이메일을 갖는 사용자는 존재하지 않습니다');
+    }
+
+    if (user.password === password) {
+      const token = generateToken(user);
+      res.json({ token });
+    } else {
+      res.status(401).send('잘못된 비밀번호입니다');
+    }
+  });
+};
+
+module.exports = { login };
