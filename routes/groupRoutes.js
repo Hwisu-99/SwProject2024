c817764fa9d00fc8782ffbc1b853866e5a833954
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-02 16:17:29 +0900

    groupRoutes.js파일에 주석 추가

--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -6,9 +6,9 @@ const authenticateJWT = require('../middleware/authenticateJWT');
 const router = express.Router();
 
 // GET : http://localhost:3000/api/groups : 인증된 사용자가 속한 그룹과 그룹 멤버 정보를 조회
-router.get('/',  group_process.getGroup);
-// authenticateJWT,
+router.get('/', authenticateJWT, group_process.getGroup);
+
 // GET : http://localhost:3000/api/groups/schedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
-router.get('/schedule', group_process.getSchedule);
+router.get('/schedule', authenticateJWT, group_process.getSchedule);
 
 module.exports = router;
