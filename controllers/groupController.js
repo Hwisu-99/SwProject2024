ac65d3722125141f711993da6e288e40d1ee36a1
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-02 16:15:13 +0900

    groupRoutes.js groupController.js 파일 수정

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,20 +1,28 @@
 // 그룹 관련 기능을 처리하는 컨트롤러 파일 
 const Group = require('../models/Group');
 
-const getGroup = (req, res) => {
-  const userId = req.user.id;
-  Group.findByUserId(userId, (err, group) => {
-    if (err) return res.status(500).send(err);
-    res.json(group);
-  });
+// 동작과 관련된 함수를 process로 묶음
+
+const group_process = {
+  // 그룹 정보 조회
+  getGroup : (req, res) => {
+    const userId = req.user.id;
+    Group.findByUserId(userId, (err, group) => {
+      if (err) return res.status(500).send(err);
+      res.json(group);
+    });
+  },
+
+  // 그룹 시간표 조회
+  getSchedule : (req, res) => {
+    const groupId = req.user.groupId;
+    Group.getSchedule(groupId, (err, schedule) => {
+      if (err) return res.status(500).send(err);
+      res.json({ schedule });
+    });
+  }
 };
 
-const getSchedule = (req, res) => {
-  const groupId = req.user.groupId;
-  Group.getSchedule(groupId, (err, schedule) => {
-    if (err) return res.status(500).send(err);
-    res.json({ schedule });
-  });
-};
 
-module.exports = { getGroup, getSchedule };
+
+module.exports = group_process;
--- routes/groupRoutes.js
+++ routes/groupRoutes.js
@@ -1,14 +1,14 @@
 // 그룹 관련 라우트 파일
 const express = require('express');
-const { getGroup, getSchedule } = require('../controllers/groupController');
+const group_process = require('../controllers/groupController');
 const authenticateJWT = require('../middleware/authenticateJWT');
 
 const router = express.Router();
 
 // GET : http://localhost:3000/api/groups : 인증된 사용자가 속한 그룹과 그룹 멤버 정보를 조회
-router.get('/', authenticateJWT, getGroup);
-
-// GET : http://localhost:3000/api/groups/shedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
-router.get('/schedule', authenticateJWT, getSchedule);
+router.get('/',  group_process.getGroup);
+// authenticateJWT,
+// GET : http://localhost:3000/api/groups/schedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
+router.get('/schedule', group_process.getSchedule);
 
 module.exports = router;
