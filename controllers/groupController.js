3fcbc0b497f9e945f3c0d169692e108dd7e1931b
JUNseo50000 <junseo50000@gmail.com>
2024-06-03 16:53:19 +0900

    change groupController

--- controllers/groupController.js
+++ controllers/groupController.js
@@ -1,20 +1,78 @@
 // 그룹 관련 기능을 처리하는 컨트롤러 파일 
-const Group = require('../models/Group');
+const { Group, Student, Lecture } = require('../models');
+
+// const getGroup = (req, res) => {
+//   const userId = req.user.id;
+//   Group.findByUserId(userId, (err, group) => {
+//     if (err) return res.status(500).send(err);
+//     res.json(group);
+//   });
+// };
+
+// const getSchedule = (req, res) => {
+//   const groupId = req.user.groupId;
+//   Group.getSchedule(groupId, (err, schedule) => {
+//     if (err) return res.status(500).send(err);
+//     res.json({ schedule });
+//   });
+// };
+
+// module.exports = { getGroup, getSchedule };
+
+const postGroup = (req, res) => {
+  Group.create({
+    lecture_id: req.params.lecture_id,
+    user_id: req.body.user_id,
+    name: req.body.name,
+  })
+    .then(result => {
+      res.status(201).send({
+        message: "Success to create group with lecture_id=" + req.params.lecture_id
+      });
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error to create group with lecture_id=" + req.params.lecture_id
+      });
+      console.log(err);
+    });
+};
 
 const getGroup = (req, res) => {
-  const userId = req.user.id;
-  Group.findByUserId(userId, (err, group) => {
-    if (err) return res.status(500).send(err);
-    res.json(group);
-  });
+  Group.findAll({
+    where: {
+      id: req.params.group_id,
+    }
+  })
+    .then(result => {
+      res.status(201).send(result)
+    })
+    .catch(err => {
+      res.status(500).send();
+    });
 };
 
-const getSchedule = (req, res) => {
-  const groupId = req.user.groupId;
-  Group.getSchedule(groupId, (err, schedule) => {
-    if (err) return res.status(500).send(err);
-    res.json({ schedule });
-  });
+const getGroupEmptyTime = (req, res) => {
+  Lecture.findAll({
+    where: {
+      id: req.user_id,
+    },
+    order: [['startTime', 'ASC']],
+  })
+    .then(result => {
+      // todo : implement algorithm
+      // 일단 자료형 어떻게 들어오는 지 보고 구현 가능
+      // 계획
+      // 오름차순 정렬이니까, startTime 비교해가면서 공강찾으면서
+      // 반복문으로 list에 추가
+
+      // res.status(201).send(result)
+    })
+    .catch(err => {
+      res.status(500).send();
+    });
 };
 
-module.exports = { getGroup, getSchedule };
+
+module.exports = { postGroup, getGroup, getGroupEmptyTime };
+
--- routes/group.js
+++ routes/group.js
@@ -1,14 +1,19 @@
 // 그룹 관련 라우트 파일
 const express = require('express');
-const { getGroup, getSchedule } = require('../controllers/groupController');
+const { postGroup, getGroup, getGroupEmptyTime } = require('../controllers/groupController');
 const authenticateJWT = require('../middleware/authenticateJWT');
+const { authPlugins } = require('mysql2');
 
 const router = express.Router();
 
-// GET : http://localhost:3000/api/groups : 인증된 사용자가 속한 그룹과 그룹 멤버 정보를 조회
-router.get('/', authenticateJWT, getGroup);
+// // GET : http://localhost:3000/api/groups : 인증된 사용자가 속한 그룹과 그룹 멤버 정보를 조회
+// router.get('/', authenticateJWT, getGroup);
 
-// GET : http://localhost:3000/api/groups/shedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
-router.get('/schedule', authenticateJWT, getSchedule);
+// // GET : http://localhost:3000/api/groups/shedule : 인증된 사용자가 속한 그룹 멤버들의 시간표를 조회
+// router.get('/schedule', authenticateJWT, getSchedule);
+
+router.post('/:lecture_id', authenticateJWT, postGroup);
+router.get('/:group_id', authenticateJWT, getGroup);
+router.get('/:group_id/empty_time', authenticateJWT, getGroup, getGroupEmptyTime);
 
 module.exports = router;
