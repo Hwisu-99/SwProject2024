122b6fb1dd104d200e4f7d9ca9b6b8d06537e4e3
JUNseo50000 <junseo50000@gmail.com>
2024-06-06 23:29:25 +0900

    before conversation

--- routes/group.js
+++ routes/group.js
@@ -3,12 +3,55 @@ const express = require('express');
 const { postGroup, getGroup, getGroupEmptyTime } = require('../controllers/groupController');
 const { createMeeting } = require('../controllers/meetingController');
 const authenticateJWT = require('../middleware/authenticateJWT');
+const { Group } = require('../models');
 
 const router = express.Router();
 
 router.post('/:lecture_id', authenticateJWT, postGroup);
 
-router.get('/:group_id', authenticateJWT, getGroup);
+router.get('/:group_id', authenticateJWT, async (req, res, next) => {
+    Group.findAll({
+        where: {
+            id: req.params.group_id,
+        }
+    })
+        .then(result => {
+            res.status(201).send(result)
+        })
+        .catch(err => {
+            res.status(500).send();
+        });
+});
+
+// router.post('/:group_id/:student_id:', authenticateJWT, async (req, res, next) => {
+router.post('/:group_id/:student_id:', async (req, res, next) => {
+    console.log("@@@@@")
+    try {
+        const group = await Group.findOne({
+            where: {
+                id: req.params.lecture_id,
+            }
+        });
+        if (!group) {
+            return res.status(404).send({ message: "group not found" });
+        }
+
+        const student = await Student.findOne({
+            where: {
+                id: req.params.student_id,
+            }
+        });
+        if (!student) {
+            return res.status(404).send({ message: "Student not found" });
+        }
+        await group.addStudent(student);
+        res.status(201).send({ message: "Student added to group successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Error adding student to group" });
+    }
+});
+
 
 router.get('/:group_id/empty_time', authenticateJWT, getGroup, getGroupEmptyTime);
 
--- routes/lecture.js
+++ routes/lecture.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const authenticateJWT = require('../middleware/authenticateJWT');
-const { Lecture, Time } = require('../models');
+const { Lecture, Time, Student } = require('../models');
 const router = express.Router();
 
 router.get('/:lecture_id', async (req, res, next) => {
@@ -33,6 +33,34 @@ router.get('time/:lecture_id', async (req, res, next) => {
                 message: "Error to read memo with memo_id=" + req.params.lecture_id
             });
         });
+});
+
+
+router.post('/:lecture_id/:student_id', async (req, res, next) => {
+    try {
+        const lecture = await Lecture.findOne({
+            where: {
+                id: req.params.lecture_id,
+            }
+        });
+        if (!lecture) {
+            return res.status(404).send({ message: "Lecture not found" });
+        }
+
+        const student = await Student.findOne({
+            where: {
+                id: req.params.student_id,
+            }
+        });
+        if (!student) {
+            return res.status(404).send({ message: "Student not found" });
+        }
+        await lecture.addStudent(student);
+        res.status(201).send({ message: "Student added to lecture successfully" });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ message: "Error adding student to lecture" });
+    }
 });
 
 module.exports = router;
