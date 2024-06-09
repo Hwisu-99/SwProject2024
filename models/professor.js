4cc82c2f99f5f4f658a4c79b1ee030ba96a9c5f7
omjinLTS <omjin7g@gmail.com>
2024-06-10 01:03:41 +0900

    feat: cors 전체 허용

--- models/professor.js
+++ models/professor.js
@@ -1,43 +1,55 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 
 module.exports = class User extends Sequelize.Model {
   static init(sequelize) {
-    return super.init({
-      eclassID: {
-        type: Sequelize.STRING(40),
-        allowNull: true,
-        unique: true,
+    return super.init(
+      {
+        eclassID: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+          unique: true,
+        },
+        eclassPW: {
+          type: Sequelize.STRING(40),
+          allowNull: true,
+          unique: false,
+        },
+        name: {
+          type: Sequelize.STRING(15),
+          allowNull: false,
+        },
       },
-      eclassPW: {
-        type: Sequelize.STRING(40),
-        allowNull: true,
-        unique: false,
-      },
-      name: {
-        type: Sequelize.STRING(15),
-        allowNull: false,
-      },
-    }, {
-      sequelize,
-      timestamps: false,
-      underscored: false,
-      modelName: 'Professor',
-      tableName: 'professors',
-      paranoid: false,
-      charset: 'utf8',
-      collate: 'utf8_general_ci',
-    });
+      {
+        sequelize,
+        timestamps: false,
+        underscored: false,
+        modelName: "Professor",
+        tableName: "professors",
+        paranoid: false,
+        charset: "utf8",
+        collate: "utf8_general_ci",
+      }
+    );
   }
 
   static associate(db) {
     db.Professor.hasMany(db.Group, {
-      foreignKey: { name: "professor_id", allowNull: false }, sourceKey: 'id', onDelete: "cascade", onUpdate: "cascade",
+      foreignKey: { name: "professor_id", allowNull: false },
+      sourceKey: "id",
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
     db.Professor.hasMany(db.Lecture, {
-      foreignKey: { name: "professor_id", allowNull: true }, sourceKey: 'id', onDelete: "cascade", onUpdate: "cascade",
+      foreignKey: { name: "professor_id", allowNull: true },
+      sourceKey: "id",
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
     db.Professor.belongsTo(db.Major, {
-      foreignKey: { name: "major_id", allowNull: false }, sourceKey: 'id', onDelete: "cascade", onUpdate: "cascade",
+      foreignKey: { name: "major_id", allowNull: false },
+      sourceKey: "id",
+      onDelete: "cascade",
+      onUpdate: "cascade",
     });
   }
 };
--- server.js
+++ server.js
@@ -1,43 +1,46 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const path = require('path');
-const morgan = require('morgan');
+const express = require("express");
+const dotenv = require("dotenv");
+const path = require("path");
+const morgan = require("morgan");
+const cors = require("cors");
 
 dotenv.config();
 
-const bodyParser = require('body-parser');
-const pageRouter = require('./routes/page');
-const authRouter = require('./routes/auth');
-const groupRouter = require('./routes/group');
-const meetingRouter = require('./routes/meeting');
-const lectureRouter = require('./routes/lecture');
+const bodyParser = require("body-parser");
+const pageRouter = require("./routes/page");
+const authRouter = require("./routes/auth");
+const groupRouter = require("./routes/group");
+const meetingRouter = require("./routes/meeting");
+const lectureRouter = require("./routes/lecture");
 
-const { swaggerUi, swaggerSpec } = require('./swagger');
+const { swaggerUi, swaggerSpec } = require("./swagger");
 const { sequelize } = require("./models");
 
 const app = express();
-app.set('port', process.env.PORT || 8001);
+app.set("port", process.env.PORT || 8001);
+app.use(cors());
 
 app.use(bodyParser.json());
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
-app.use('/page', pageRouter);
-app.use('/auth', authRouter);
-app.use('/group', groupRouter);
-app.use('/meetings', meetingRouter);
-app.use('/lecture', lectureRouter);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/page", pageRouter);
+app.use("/auth", authRouter);
+app.use("/group", groupRouter);
+app.use("/meetings", meetingRouter);
+app.use("/lecture", lectureRouter);
 
 // 데이터베이스 동기화 (force로 설정시 데이터베이스의 테이블을 항상 새로 생성(기존 테이블 삭제하고 새로 만듦))
-// force: true -> re-create new table whenever ERD is chenaged. 
-sequelize.sync({ force: false })
+// force: true -> re-create new table whenever ERD is chenaged.
+sequelize
+  .sync({ force: false })
   .then(() => {
-    console.log('DB connecdtion success');
+    console.log("DB connecdtion success");
   })
   .catch((err) => {
     console.error(err);
   });
 
-app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(morgan("dev"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -50,11 +53,10 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
   res.locals.message = err.message;
-  res.locals.error = process.env.NODE_ENV !== 'production' ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
 });
 
-app.listen(app.get('port'), () => {
-  console.log('Waiting at PORT : ', app.get('port'));
+app.listen(app.get("port"), () => {
+  console.log("Waiting at PORT : ", app.get("port"));
 });
-
