6ba98fc05ec93e0e92814f22cc599a8270939580
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-06-10 12:18:36 +0900

    authenticateJWT.js syntax fixed

--- middleware/authenticateJWT.js
+++ middleware/authenticateJWT.js
@@ -4,8 +4,6 @@ const jwt = require('jsonwebtoken');
 const authenticateJWT = (req, res, next) => {
   const authHeader = req.headers.authorization;
   console.log(authHeader);
-  if (authHeader) {
-
     let token;
     if (authHeader) {
       if (authHeader.startsWith('Bearer ')) {
