33a950666ccb9e9c4995bda0a8dc90824a962d64
COFFEEB0Y <108715839+COFFEEB0Y@users.noreply.github.com>
2024-07-01 23:20:17 +0900

    update jwt.js

--- utils/jwt.js
+++ utils/jwt.js
@@ -2,26 +2,12 @@ const jwt = require('jsonwebtoken');
 
 // 토큰 생성
 const generateToken = (user) => {
-<<<<<<< HEAD
   // 토큰의 헤드를 사용자의 이름과 아이디로 구성 
   return jwt.sign(
     {name: user.name, eclassID: user.eclassID}, 
     process.env.JWT_SECRET, {
     expiresIn: '1h',
   });
-=======
-  // req.user의 구조 결정하는 부분 -> user table의 id, email name, 나중에 user가 속한 groupId 추가 
-  return jwt.sign(
-    { 
-      id: user.id, 
-      email: user.email, 
-      name: user.name, 
-      user_group_id: user.user_group_id 
-    }, 
-    process.env.JWT_SECRET, 
-    { expiresIn: '1h',}
-  );
->>>>>>> new-origin/main
 };
 
 // 토큰 검사
